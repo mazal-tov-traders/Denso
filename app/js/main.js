95f1b9f55efecadfec6edbb2e94cc0941d3fe6be
mazal-tov-traders <d90.german@gmail.com>
2022-11-11 20:51:37 +0200

    total project

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+
+  
+  $('.slaid__wraper').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 675,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+    
+      
+
+
+  });
+
+
+
+});
+
+  
+
